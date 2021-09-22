07c1348de6eedb08d65c21d0e5d839b0164097bf
SAY-droid427 <sayanimallick0218@gmail.com>
2021-09-22 12:30:10 +0530

    Handling missing data

--- Data Preprocessing/data_processing.py
+++ Data Preprocessing/data_processing.py
@@ -8,5 +8,10 @@ dataset = pd.read_csv('/home/sayani/Desktop/DeepLearning/Data Preprocessing/Data
 X = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,3].values
 
+# Taking care of the missing data
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean',verbose=0)
+imputer = imputer.fit(X[:,1:3])
+X[:,1:3] = imputer.transform(X[:,1:3])
+
 print(X)
-print(y)
